62412bd13da18d8756adf9a113dfaa00e2aaf3ce
roybarak80 <barakroy@gmail.com>
2018-10-12 02:06:27 +0300

    Updates

---
+++ precache-manifest.814879a24994b8a169e999f52db7e2c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "9b38bbba7fa7a3927c13",
+    "url": "/react-gh-pages/static/js/main.9b38bbba.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/react-gh-pages/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "9b38bbba7fa7a3927c13",
+    "url": "/react-gh-pages/static/css/main.224e68a6.chunk.css"
+  },
+  {
+    "revision": "674818275871310617f0c18f56003ca4",
+    "url": "/react-gh-pages/index.html"
+  }
+];
